4db1d440ac274afe58a46a0c4a68098213796427
xuanjinliang <xuanjinliang@gmail.com>
2022-05-02 21:34:09 +0800

    性能分析

--- performance_test.go
+++ performance_test.go
@@ -9,7 +9,7 @@ import (
 	"unsafe"
 )
 
-// 测试map的value用unsafe.Pointer与interface比较
+// test map value ues unsafe.Pointer and interface type compare
 func BenchmarkUnsafePointAndInterfaceA(b *testing.B) {
 	b.ResetTimer()
 	for j := 0; j < b.N; j++ {
@@ -32,7 +32,7 @@ func BenchmarkUnsafePointAndInterfaceB(b *testing.B) {
 	b.StopTimer()
 }
 
-// 测试map key用string，key，interface比较
+// test map key use int、string and interface type compare
 func BenchmarkIntStringInterfaceA(b *testing.B) {
 	sliceList := make([]int, 0, 100000)
 	for i := 0; i < cap(sliceList); i++ {
@@ -81,7 +81,7 @@ func BenchmarkIntStringInterfaceC(b *testing.B) {
 	b.StopTimer()
 }
 
-// list与slice比较
+// list and slice add compare
 func BenchmarkListAndSliceInsertA(b *testing.B) {
 	b.ResetTimer()
 	sliceList := make([]int, 0)
@@ -95,49 +95,110 @@ func BenchmarkListAndSliceInsertA(b *testing.B) {
 	b.StopTimer()
 }
 
+func TestListAndSliceInsertA(t *testing.T) {
+	sliceList := make([]int, 0)
+
+	for i := 0; i < 10; i++ {
+		sliceList = append(sliceList, i)
+	}
+	t.Logf("%v", sliceList)
+}
+
 func BenchmarkListAndSliceInsertB(b *testing.B) {
 	b.ResetTimer()
 	containerList := list.New()
 	for j := 0; j < b.N; j++ {
 		for i := 0; i < 10000; i++ {
-			containerList.PushFront(i)
+			containerList.PushBack(i)
 		}
 	}
 	b.StopTimer()
 }
 
+func TestListAndSliceInsertB(t *testing.T) {
+	containerList := list.New()
+	for i := 0; i < 10; i++ {
+		containerList.PushBack(i)
+	}
+	for e := containerList.Front(); e != nil; e = e.Next() {
+		t.Logf("%v", e.Value.(int))
+	}
+}
+
+// list and slice remove compare
 func BenchmarkListAndSliceRemoveA(b *testing.B) {
-	num := 10000000
+	num := 100
 	sliceList := make([]int, 0)
 	for i := 0; i < num; i++ {
 		sliceList = append(sliceList, i)
 	}
 
 	b.ResetTimer()
-	for i := 0; i < num; i++ {
-		sliceList = sliceList[1:len(sliceList)]
+	for j := 0; j < b.N; j++ {
+		a := sliceList[:]
+		for i := 0; i < num; i++ {
+			a = a[1:len(a)]
+		}
 	}
 	b.StopTimer()
 }
 
+func TestListAndSliceRemoveA(t *testing.T) {
+	num := 10
+	sliceList := make([]int, 0)
+	for i := 0; i < num; i++ {
+		sliceList = append(sliceList, i)
+	}
+
+	a := sliceList[:]
+	for i := 0; i < num; i++ {
+		a = a[1:len(a)]
+
+		t.Logf("%v", a)
+	}
+}
+
 func BenchmarkListAndSliceRemoveB(b *testing.B) {
-	num := 10000000
+	num := 100
 	containerList := list.New()
 	for i := 0; i < num; i++ {
 		containerList.PushFront(i)
 	}
 
 	b.ResetTimer()
-	var prev *list.Element
-	for e := containerList.Back(); e != nil; e = prev {
-		prev = e.Prev()
-		containerList.Remove(e)
+	for j := 0; j < b.N; j++ {
+		copyContainerList := list.New()
+		copyContainerList.PushBackList(containerList)
+		var prev *list.Element
+		for e := copyContainerList.Back(); e != nil; e = prev {
+			prev = e.Prev()
+			copyContainerList.Remove(e)
+		}
 	}
 	b.StopTimer()
 }
 
-func BenchmarkListAndSliceRandomInsertA(b *testing.B) {
+func TestListAndSliceRemoveB(t *testing.T) {
 	num := 10
+	containerList := list.New()
+	for i := 0; i < num; i++ {
+		containerList.PushFront(i)
+	}
+
+	copyContainerList := list.New()
+	copyContainerList.PushBackList(containerList)
+	var prev *list.Element
+	for e := copyContainerList.Back(); e != nil; e = prev {
+		t.Logf("%v", e.Value.(int))
+		prev = e.Prev()
+		copyContainerList.Remove(e)
+	}
+	t.Logf("len --> %v", copyContainerList.Len())
+}
+
+// If you know the index, list and slice random add compare
+func BenchmarkListAndSliceRandomIndexInsertA(b *testing.B) {
+	num := 10000
 	sliceList := make([]int, 0)
 	for i := 0; i < num; i++ {
 		sliceList = append(sliceList, i)
@@ -146,7 +207,7 @@ func BenchmarkListAndSliceRandomInsertA(b *testing.B) {
 	for j := 0; j < b.N; j++ {
 		l := len(sliceList)
 		rand.Seed(time.Now().UnixNano())
-		randNum := rand.Intn(l)
+		randNum := rand.Intn(l) // random index
 		newSlice := make([]int, l+1)
 		copy(newSlice, sliceList[:randNum])
 		newSlice[randNum] = randNum
@@ -157,8 +218,29 @@ func BenchmarkListAndSliceRandomInsertA(b *testing.B) {
 	b.StopTimer()
 }
 
-func BenchmarkListAndSliceRandomInsertB(b *testing.B) {
+func TestListAndSliceRandomIndexInsertA(t *testing.T) {
 	num := 10
+	sliceList := make([]int, 0)
+	for i := 0; i < num; i++ {
+		sliceList = append(sliceList, i)
+	}
+
+	for j := 0; j < 2; j++ {
+		l := len(sliceList)
+		rand.Seed(time.Now().UnixNano())
+		randNum := rand.Intn(l) // random index
+		t.Logf("randNum --> %v", randNum)
+		newSlice := make([]int, l+1)
+		copy(newSlice, sliceList[:randNum])
+		newSlice[randNum] = randNum
+		copy(newSlice[randNum+1:], sliceList[randNum:])
+		sliceList = newSlice
+	}
+	t.Logf("%v", sliceList)
+}
+
+func BenchmarkListAndSliceRandomIndexInsertB(b *testing.B) {
+	num := 10000
 	containerList := list.New()
 	for i := 0; i < num; i++ {
 		containerList.PushBack(i)
@@ -170,18 +252,237 @@ func BenchmarkListAndSliceRandomInsertB(b *testing.B) {
 		rand.Seed(time.Now().UnixNano())
 		randNum := rand.Intn(l)
 
+		index := 0
 		for e := containerList.Front(); e != nil; e = e.Next() {
-			if e.Value.(int) == randNum {
+			if index == randNum {
 				ef := containerList.PushBack(randNum)
 				containerList.MoveBefore(ef, e)
 				break
+			} else {
+				index += 1
 			}
 		}
 	}
 	b.StopTimer()
 }
 
-// list 循环删除方式
+func TestListAndSliceRandomIndexInsertB(t *testing.T) {
+	num := 10
+	containerList := list.New()
+	for i := 0; i < num; i++ {
+		containerList.PushBack(i)
+	}
+
+	for j := 0; j < 2; j++ {
+		l := containerList.Len()
+		rand.Seed(time.Now().UnixNano())
+		randNum := rand.Intn(l) // random index
+		t.Logf("randNum --> %v", randNum)
+
+		index := 0
+		for e := containerList.Front(); e != nil; e = e.Next() {
+			if index == randNum {
+				ef := containerList.PushBack(randNum)
+				containerList.MoveBefore(ef, e)
+				break
+			} else {
+				index += 1
+			}
+		}
+	}
+
+	for e := containerList.Front(); e != nil; e = e.Next() {
+		t.Logf("%v", e.Value.(int))
+	}
+}
+
+// If you know the index, list and slice random remove compare
+func BenchmarkListAndSliceRandomIndexRemoveA(b *testing.B) {
+	num := 10000
+	sliceList := make([]int, 0)
+	for i := 0; i < num; i++ {
+		sliceList = append(sliceList, i)
+	}
+
+	b.ResetTimer()
+	for j := 0; j < b.N; j++ {
+		a := sliceList[:]
+
+		for i := 0; i < num/2; i++ {
+			l := len(a)
+			rand.Seed(time.Now().UnixNano())
+			randNum := rand.Intn(l) // random index
+			newSlice := make([]int, l-1)
+			copy(newSlice, a[:randNum])
+			copy(newSlice[randNum:], a[randNum+1:])
+			a = newSlice
+		}
+
+	}
+	b.StopTimer()
+}
+
+func TestListAndSliceRandomIndexRemoveA(t *testing.T) {
+	num := 10
+	sliceList := make([]int, 0)
+	for i := 0; i < num; i++ {
+		sliceList = append(sliceList, i)
+	}
+
+	a := sliceList[:]
+
+	for i := 0; i < num/2; i++ {
+		l := len(a)
+		rand.Seed(time.Now().UnixNano())
+		randNum := rand.Intn(l) // random index
+		t.Logf("randNum --> %v", randNum)
+		newSlice := make([]int, l-1)
+		copy(newSlice, a[:randNum])
+		copy(newSlice[randNum:], a[randNum+1:])
+		a = newSlice
+	}
+	t.Logf("%v", a)
+}
+
+func BenchmarkListAndSliceRandomIndexRemoveB(b *testing.B) {
+	num := 10000
+	containerList := list.New()
+	for i := 0; i < num; i++ {
+		containerList.PushBack(i)
+	}
+
+	b.ResetTimer()
+	for j := 0; j < b.N; j++ {
+		copyContainerList := list.New()
+		copyContainerList.PushBackList(containerList)
+
+		for i := 0; i < num/2; i++ {
+			l := copyContainerList.Len()
+			rand.Seed(time.Now().UnixNano())
+			randNum := rand.Intn(l)
+
+			index := 0
+			for e := copyContainerList.Front(); e != nil; e = e.Next() {
+				if index == randNum {
+					copyContainerList.Remove(e)
+					break
+				} else {
+					index += 1
+				}
+			}
+		}
+	}
+	b.StopTimer()
+}
+
+func TestListAndSliceRandomIndexRemoveB(t *testing.T) {
+	num := 10
+	containerList := list.New()
+	for i := 0; i < num; i++ {
+		containerList.PushBack(i)
+	}
+
+	copyContainerList := list.New()
+	copyContainerList.PushBackList(containerList)
+
+	for i := 0; i < num/2; i++ {
+		l := copyContainerList.Len()
+		rand.Seed(time.Now().UnixNano())
+		randNum := rand.Intn(l)
+
+		t.Logf("randNum --> %v", randNum)
+
+		index := 0
+		for e := copyContainerList.Front(); e != nil; e = e.Next() {
+			if index == randNum {
+				copyContainerList.Remove(e)
+				break
+			} else {
+				index += 1
+			}
+		}
+	}
+
+	for e := copyContainerList.Front(); e != nil; e = e.Next() {
+		t.Logf("%v", e.Value.(int))
+	}
+}
+
+func TestListAndSliceRandomValueInsertB(t *testing.T) {
+	num := 10
+	containerList := list.New()
+	for i := 0; i < num; i++ {
+		containerList.PushBack(i)
+	}
+
+	for j := 0; j < 2; j++ {
+		l := containerList.Len()
+		rand.Seed(time.Now().UnixNano())
+		randNum := rand.Intn(l) // random index
+		t.Logf("randNum --> %v", randNum)
+
+		index := 0
+		for e := containerList.Front(); e != nil; e = e.Next() {
+			if index == randNum {
+				ef := containerList.PushBack(randNum)
+				containerList.MoveBefore(ef, e)
+				break
+			} else {
+				index += 1
+			}
+		}
+	}
+
+	for e := containerList.Front(); e != nil; e = e.Next() {
+		t.Logf("%v", e.Value.(int))
+	}
+}
+
+type IntBig struct {
+	Num1 int64
+	Num2 int64
+	Num3 int64
+	Num4 int64
+	Num5 int64
+}
+
+func BenchmarkListAndSliceBigInsertA(b *testing.B) {
+	b.ResetTimer()
+	sliceList := make([]IntBig, 0)
+
+	for j := 0; j < b.N; j++ {
+		for i := 0; i < 1000; i++ {
+			sliceList = append(sliceList, IntBig{
+				Num1: int64(i),
+				Num2: int64(i),
+				Num3: int64(i),
+				Num4: int64(i),
+				Num5: int64(i),
+			})
+		}
+	}
+
+	b.StopTimer()
+}
+
+func BenchmarkListAndSliceBigInsertB(b *testing.B) {
+	b.ResetTimer()
+	containerList := list.New()
+	for j := 0; j < b.N; j++ {
+		for i := 0; i < 1000; i++ {
+			containerList.PushBack(IntBig{
+				Num1: int64(i),
+				Num2: int64(i),
+				Num3: int64(i),
+				Num4: int64(i),
+				Num5: int64(i),
+			})
+		}
+	}
+	b.StopTimer()
+}
+
+// list for remove element
 func TestSliceList(t *testing.T) {
 	containerList := list.New()
 	for i := 0; i < 10; i++ {
@@ -199,52 +500,4 @@ func TestSliceList(t *testing.T) {
 	}
 
 	t.Logf("len --> %v", containerList.Len())
-}
-
-// slice 随机添加
-func TestSliceRandomInsert(t *testing.T) {
-	num := 10
-	sliceList := make([]int, 0)
-	for i := 0; i < num; i++ {
-		sliceList = append(sliceList, i)
-	}
-
-	l := len(sliceList)
-	rand.Seed(time.Now().UnixNano())
-	randNum := rand.Intn(l)
-	t.Logf("randNum --> %v", randNum)
-
-	newSlice := make([]int, l+1)
-
-	copy(newSlice, sliceList[:randNum])
-	newSlice[randNum] = randNum
-	copy(newSlice[randNum+1:], sliceList[randNum:])
-
-	t.Logf("%v", newSlice)
-}
-
-// slice 随机删除
-func TestListRandomInsert(t *testing.T) {
-	num := 10
-	containerList := list.New()
-	for i := 0; i < num; i++ {
-		containerList.PushBack(i)
-	}
-
-	l := containerList.Len()
-	rand.Seed(time.Now().UnixNano())
-	randNum := rand.Intn(l)
-	t.Logf("randNum --> %v", randNum)
-
-	for e := containerList.Front(); e != nil; e = e.Next() {
-		if e.Value.(int) == randNum {
-			ef := containerList.PushBack(randNum)
-			containerList.MoveBefore(ef, e)
-			break
-		}
-	}
-
-	for e := containerList.Front(); e != nil; e = e.Next() {
-		t.Logf("%v", e.Value.(int))
-	}
 }
